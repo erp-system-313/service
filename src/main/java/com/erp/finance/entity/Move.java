14e575f93f9d40f5ea4578ccf24dc56bb822f5e8
OmarHesham2356 <o.hesham2356@nu.edu.eg>
2026-05-16 15:33:03 +0300

    fix(finance): fix InvoiceRepository EntityGraph referencing non-existent fields
    - Removed 'lines' and 'salesOrder' from @EntityGraph in findByIdWithPayments
    - Invoice entity doesn't have these fields (lines are in InvoiceLine entity, salesOrder is transient)
    - Only 'customer' is a valid entity graph path on Invoice

--- src/main/java/com/erp/finance/entity/Move.java
+++ src/main/java/com/erp/finance/entity/Move.java
@@ -1,5 +1,6 @@
 package com.erp.finance.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
@@ -142,6 +143,7 @@ public class Move {
 
     @OneToMany(mappedBy = "move", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
+    @JsonIgnore
     private List<MoveLine> lines = new ArrayList<>();
 
     @CreationTimestamp
